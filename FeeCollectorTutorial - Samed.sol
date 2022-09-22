82a5bea2b2fa72c2067c93dbc0241c687caf3945
Samed Torun <33091148+samedtorunn@users.noreply.github.com>
2022-09-22 18:44:15 +0300

    Update FeeCollectorTutorial - Samed.sol

--- FeeCollectorTutorial - Samed.sol
+++ FeeCollectorTutorial - Samed.sol
@@ -1,25 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity 0.8.7; // solidity version is declared.
 
 contract FeeCollector {
-    address public owner;
-    uint256 public balance;
+    address public owner; // this will be set as the contract owner's address later.
+    uint256 public balance; // this will be defining the balance of the deposits.
 
-    constructor() {
+    constructor() { // basic constructor function.
         owner = msg.sender; // sets the owner as the deployer adress.
     }
 
-    receive() payable external {
+    receive() payable external { // this is payable since wei is transferred.
         balance += msg.value; // Message value is represented in wei.
     }
 
 
 
-    function withdraw(uint amount, address payable destAddr) public {
-        require(msg.sender == owner, "Only owners can request withdraw");
-        require(amount <= balance, "Funds are not sufficient");
+    function withdraw(uint amount, address payable destAddr) public { // money withdrawal function. 
+        require(msg.sender == owner, "Only owners can request withdraw"); // requirement and the error if the requirement does not satisfied.
+        require(amount <= balance, "Funds are not sufficient"); // requirement and the error if the requirement does not satisfied.
 
-        destAddr.transfer(amount);
+        destAddr.transfer(amount); //amount gets transferred.
         balance -= amount; // balance is decreased by the amount given.
     }
 }
